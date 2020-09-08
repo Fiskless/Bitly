4c48ce86c6126056b81f690ce1f847db5395720e
Fiskless <70478482+Fiskless@users.noreply.github.com>
2020-09-09 01:23:24 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Bitly url shorterer
+
+This project uses the Bitly API, which allows you to create shorten links. When the user input a link, a shortened link is displayed. But if the user specified a short link, it shows statistics of clicks on it.
+The result of code completion is presented below
+
+### How to install
+
+First of all, you should create account on bitly.com. Then you need to get access token. For this purpose, log in your account, then select profile settings > Generic access Token. Enter your password and token will be generated automatically. You will get a token string similar to 99c09e20aa122225122fc1977542fecf77771da7.
+
+Python3 should be already installed. 
+Then use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
+```
+pip install -r requirements.txt
+```
+
+### Project Goals
+
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
---
+++ main.py
@@ -0,0 +1,54 @@
+import requests
+from urllib.parse import urlparse
+import os
+from dotenv import load_dotenv
+import argparse
+
+
+load_dotenv()
+SECRET_TOKEN = os.getenv("BITLY_API_TOKEN")
+
+
+def shorten_link(token, url):
+    response = requests.post('https://api-ssl.bitly.com/v4/bitlinks',
+                             headers = {'Authorization': f'Bearer {token}'},
+                             json = {"long_url": url})
+    response.raise_for_status()
+    link_shorten = response.json()['link']
+    return link_shorten
+
+
+def count_clicks(token, bitlink):
+    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary',
+                            headers = {'Authorization': f'Bearer {token}'},
+                            params = {"units": '-1'})
+    response.raise_for_status()              
+    clicks_count = response.json()["total_clicks"]
+    return clicks_count
+
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('link', nargs='?')
+    return parser
+
+
+if __name__ == '__main__':
+    try:
+        parser = createParser()
+        args = parser.parse_args()
+        if args.link:
+            your_link = args.link
+        else:
+            your_link = input('Input the link: ')
+
+        check_on_bitlink = your_link.startswith("https://bit.ly")
+        if check_on_bitlink:
+            parsed = urlparse(your_link)
+            print("Count clicks of your bitlink is", count_clicks(SECRET_TOKEN, f'{parsed.netloc}{parsed.path}'))
+        else:
+            short_link = shorten_link(SECRET_TOKEN,your_link)
+            print('Yout bitlink is', short_link)
+    except:
+        print("Your URL is not correct")
+
